834015b8c55209bfa56a80eed3c90c7dd42f95d1
Emir Arı <emirariomatic@gmail.com>
2022-08-03 13:05:22 +0300

    Initial Commit

---
+++ Just Sign In App/Just_Sign_In_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Just_Sign_In_AppApp.swift
+//  Just Sign In App
+//
+//  Created by Emir on 3.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Just_Sign_In_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
