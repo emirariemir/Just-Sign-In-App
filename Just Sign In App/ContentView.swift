4c3c308e1685744a348b99da3374744d0bc03b0c
Emir Arı <emirariomatic@gmail.com>
2022-08-03 14:00:11 +0300

    Complete First page

--- Just Sign In App/ContentView.swift
+++ Just Sign In App/ContentView.swift
@@ -9,13 +9,65 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            
+            Color("Background Color").edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                
+                CustomImage(imageName: "Human")
+                    .padding(.bottom, 10)
+                
+                Spacer()
+                
+                PrimaryButton(title: "Get Started", bgColorName: "Primary Color")
+                    .padding(.bottom, 10)
+                
+                PrimaryButton(title: "Sign In", bgColorName: "Text Color")
+                    .padding(.bottom, 10)
+                
+                HStack{
+                    Text("Want to learn more?")
+                    Text("Discover here.")
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("Text Color"))
+                }.padding(.bottom, 20)
+                
+            }.padding([.leading, .trailing], 20)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct PrimaryButton: View {
+    var title: String
+    var bgColorName: String
+    
+    var body: some View {
+        Text(title)
+            .font(.title)
+            .fontWeight(.bold)
+            .foregroundColor(Color.white)
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(Color(bgColorName))
+            .cornerRadius(50)
+    }
+}
+
+struct CustomImage: View {
+    var imageName: String
+    
+    var body: some View {
+        Image(imageName)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 350, height: 350, alignment: .center)
     }
 }
